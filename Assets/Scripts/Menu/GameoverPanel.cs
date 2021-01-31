28583953cdefecfe3e988b3d50fd61355d37cbc8
Yan Jardim <yan.guilherme72@gmail.com>
2021-01-31 14:45:55 -0300

    victory

--- Assets/Scripts/Menu/GameoverPanel.cs
+++ Assets/Scripts/Menu/GameoverPanel.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameoverPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void OnEnable()
-    {
+	public AudioClip success;
+	public Sprite secondImage;
+	private Image image;
+	// Start is called before the first frame update
+	void OnEnable()
+	{
+		AudioManager.Instance.PlaySFX(success);
 		transform.localScale = new Vector3(0, 0, 0);
-        LeanTween.scale(GetComponent<RectTransform>(), new Vector3(1, 1, 1), 1.2f).setEase(LeanTweenType.easeInBounce);
-    }
+		LeanTween.scale(GetComponent<RectTransform>(), new Vector3(1, 1, 1), 1.2f).setEase(LeanTweenType.easeInBounce);
+		Invoke("ChangeImage", 2.0f);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void ChangeImage()
+	{
+		transform.localScale = new Vector3(0, 0, 0);
+		LeanTween.scale(GetComponent<RectTransform>(), new Vector3(1, 1, 1), 1.2f).setEase(LeanTweenType.easeInBounce);
+		image.sprite = secondImage;
+	}
 }
